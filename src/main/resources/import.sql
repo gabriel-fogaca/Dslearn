5156373ed006a50cab332c50580e2cf283a7f00f
gabriel-fogaca <l44.gabriel@hotmail.com>
2025-09-30 13:04:09 -0300

    Notification

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -18,3 +18,4 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
 
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Olá',NOW(),false,'http://localhost:8080/',1);
